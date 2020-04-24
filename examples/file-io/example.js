8d0f8b20a66a4585e298f5a5460ba446b8454809
Mason Mackaman <masondeanm@aol.com>
2020-04-23 20:25:59 -0400

    Change formatting

--- examples/file-io/example.js
+++ examples/file-io/example.js
@@ -1,12 +1,11 @@
 'use strict';
 const
 	{promises: fsp} = require(`fs`),
-	jim = require(`@ursi/jim`);
-
-
-const {Elm} = require(`./elm`);
+	jim = require(`@ursi/jim`),
+	{Elm} = require(`./elm`);
 
 jim.regTask(`writeFile`, fsp.writeFile);
+
 jim.regTask(`readFile`, async filePath => {
 	try {
 		const text = await fsp.readFile(filePath, `utf-8`);
